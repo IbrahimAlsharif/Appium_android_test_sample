d99208270bcdb6779f61b6602055a926ed8278ae
Ibrahim Alsharif <ibrahimht2000@gmail.com>
2022-12-07 13:08:15 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main App jjj");
+        System.out.println("Main App");
 
     }
 }
